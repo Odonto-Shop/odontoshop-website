47a536a7f56818261887956aaed2b46a8d79c6c3
Lauriel Mesquita <laurielmesquita@me.com>
2021-05-20 14:11:13 -0300

    :rocket: Deploy stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,7 @@ const IndexPage = () => (
     <Seo title='Home' />
     <h1>Olá visitante</h1>
     <p>Bem vindos ao site da Odonto Shop.</p>
-    <p>
-      Estamos preparando um website completo onde você encontrará tudo que
-      precisa em breve.
-    </p>
+    <p>Em breve você encontrará um website completo.</p>
   </Layout>
 )
 
